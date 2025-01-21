45cb074b0d40dce450381755b606e2a9df8472b0
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2025-01-21 23:16:46 +0200

    feat: added basic hello world api

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-func main(){
-	fmt.Println("hello world");
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+    r := chi.NewRouter()
+    r.Use(middleware.Logger)
+    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello World!"))
+    })
+    http.ListenAndServe(":3000", r)
 }
--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module movez.com
 
 go 1.21.3
+
+require github.com/go-chi/chi/v5 v5.2.0
