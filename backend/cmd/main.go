122cd2c9e0f8f1262e2ccfabf1f517016eadba2a
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2025-01-22 19:09:10 +0200

    chore(cmd): updated router to fiber

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,17 +1,13 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-)
+import "github.com/gofiber/fiber/v2"
 
 func main() {
-    r := chi.NewRouter()
-    r.Use(middleware.Logger)
-    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Hello World!"))
-    })
-    http.ListenAndServe(":3000", r)
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
 }
