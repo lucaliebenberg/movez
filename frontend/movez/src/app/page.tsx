6c9159039d77c63e7ab5b6df514d89f81a9056e8
James-Frown <21122@ctca.co.za>
2025-01-22 21:56:23 +0200

    feat: added basic nav and some styling

--- frontend/movez/src/app/page.tsx
+++ frontend/movez/src/app/page.tsx
@@ -1,3 +1,12 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
-  return <div className="flex items-center mx-auto">Hello World</div>;
+  return (
+    <div className=" bg-[#059669] text-white">
+      <div className="w-full h-full">
+        <Navbar />
+        <div className="flex flex-col items-center w-full h-full">Content</div>
+      </div>
+    </div>
+  );
 }
---
+++ frontend/movez/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div className="flex justify-between px-5 py-2">
+      <div className="flex gap-5">
+        <button>logo</button>
+        <h1>Movez</h1>
+      </div>
+      <button>sign out btn</button>
+    </div>
+  );
+}
