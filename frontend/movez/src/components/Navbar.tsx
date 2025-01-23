17a3acce5fde40769fef920f01d43e38c98f912e
James-Frown <21122@ctca.co.za>
2025-01-23 14:40:38 +0200

    chore: updating parts of the nav

--- frontend/movez/src/components/Navbar.tsx
+++ frontend/movez/src/components/Navbar.tsx
@@ -1,11 +1,22 @@
+import ExitIcon from "@/assets/Icon-Exit.svg";
+import MovezLogo from "@/assets/Logo-Moves.svg";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <div className="flex justify-between px-5 py-2">
-      <div className="flex gap-5">
-        <button>logo</button>
-        <h1>Movez</h1>
-      </div>
-      <button>sign out btn</button>
+      <button className="flex items-center px-1 py-1">
+        <Link href="/" className="flex gap-1 items-center">
+          <Image src={MovezLogo} alt="Movez Logo" width={40} height={40} />
+          <h1 className="font-bold font-serif">Movez</h1>
+        </Link>
+      </button>
+      <button className="flex items-center px-1 py-1">
+        <Link href="/">
+          <Image src={ExitIcon} alt="Exit Icon" width={40} height={40} />
+        </Link>
+      </button>
     </div>
   );
 }
