b0915d6f19e666ae2aaec9c44a3180db36468148
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2025-02-09 21:35:08 +0200

    chore: errors impl

--- backend/api/errors.go
+++ backend/api/errors.go
@@ -1,13 +1,60 @@
 package api
 
-type UserHandler struct {}
+import (
+	"net/http"
 
-func NewUserHandler() {}
+	"github.com/gofiber/fiber/v2"
+)
 
-func HandleDeleteUser() {}
+func ErrorHandler(c *fiber.Ctx, err error) error {
+	if apiError, ok := err.(Error); ok {
+		return c.Status(apiError.Code).JSON(apiError)
+	}
+	apiError := NewError(http.StatusInternalServerError, err.Error())
+	return c.Status(apiError.Code).JSON(apiError)
+}
 
-func HandlePutUser() {}
+type Error struct {
+	Code int    `json:"code"`
+	Err  string `json:"error"`
+}
 
-func HandlePostUser() {}
+// Error implements the Error interface
+func (e Error) Error() string {
+	return e.Err
+}
 
-func HandleGetUser() {}
+func NewError(code int, err string) Error {
+	return Error{
+		Code: code,
+		Err:  err,
+	}
+}
+
+func ErrUnauthorized() Error {
+	return Error{
+		Code: http.StatusBadRequest,
+		Err:  "Unauthorized request",
+	}
+}
+
+func ErrResourceNotFound(res string) Error {
+	return Error{
+		Code: http.StatusNotFound,
+		Err:  res + "resource not found",
+	}
+}
+
+func ErrBadRequest() Error {
+	return Error{
+		Code: http.StatusBadRequest,
+		Err:  "Invalid JSON request",
+	}
+}
+
+func ErrInvalidID() Error {
+	return Error{
+		Code: http.StatusBadRequest,
+		Err:  "Invalid ID given",
+	}
+}
